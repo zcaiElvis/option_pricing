e265d8be7b617ed613ae302d197702159ba00652
Elvis Cai <elvis@dhcp-128-189-249-165.ubcsecure.wireless.ubc.ca>
2024-03-15 14:17:44 -0700

    updated exp finite diff

--- src/explicit_finite_diff.py
+++ src/explicit_finite_diff.py
@@ -30,35 +30,32 @@ class finite_difference:
         return 1/(1+self.r * self.delta_T) * (0.5*(self.r-self.q)*j*self.delta_T + 0.5 * self.sigma**2 * j**2 * self.delta_T)
     
     
-    def create_grid(self, K_range = tc.tensor([0, 100])):
+    def create_grid(self, S_range = tc.tensor([0, 100])):
         self.Ts = tc.arange(0, self.T + self.delta_T, self.delta_T)
-        self.Ks = tc.arange(K_range[0], K_range[1] + self.delta_K, self.delta_K)
+        self.Ss = tc.arange(S_range[0], S_range[1] + self.delta_K, self.delta_K)
         
-        a = [self.a_star(j) for j in range(len(self.Ks))]
-        b = [self.b_star(j) for j in range(len(self.Ks))]
-        c = [self.c_star(j) for j in range(len(self.Ks))]
+        a = [self.a_star(j) for j in range(len(self.Ss))]
+        b = [self.b_star(j) for j in range(len(self.Ss))]
+        c = [self.c_star(j) for j in range(len(self.Ss))]
         
-        M = len(self.Ks)
+        M = len(self.Ss)
         N = len(self.Ts)
         
         f = tc.empty(N, M)
         
-        f[-1,:] = tc.tensor([tc.max(tc.tensor(K - S), tc.tensor(0.)) for S in K])
-        f[:, 0] = K
+        f[-1,:] = tc.tensor([tc.max(tc.tensor(self.K - S), tc.tensor(0.)) for S in self.Ss])
+        f[:, 0] = self.K
         f[:, -1] = 0
         
         
         for i in reversed(range(N - 1)):
-            for j in range(M - 1):
-                print("##")
-                print(i)
-                print(j)
+            for j in range(1, M - 1):
+                f[i, j] = a[j] * f[i+1, j-1] + b[j] * f[i+1, j] + c[j] * f[i+1, j+1]
         
-        return
+        return f
         
         
 if __name__ == "__main__":
     
     fd = finite_difference(0.1, 0, 0.2, 50, 0.4167, 5, 0.0417)
-    
     fd.create_grid()
